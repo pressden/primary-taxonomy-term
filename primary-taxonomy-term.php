03eb371feff7543ff8c3db841e2d77c80ac60781
D.S. Webster <dswebs.me@gmail.com>
2018-02-22 11:54:44 -0500

    Add required plugin file so WordPress can see it

---
+++ primary-taxonomy-term.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Primary Taxonomy Term
+Plugin URI: https://github.com/pressden/primary-taxonomy-term
+Description: Primary Taxonomy Term adds the ability to specify a primary term for any taxonomy.
+Version: 0.1.0
+Author: D.S. Webster
+Author URI: http://pressden.com/
+License: GPLv3
+Text Domain: primary-taxonomy-term
+*/
