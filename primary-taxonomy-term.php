51bb9e5ad67172650aba7e6a0cc37491b6dca788
D.S. Webster <dswebs.me@gmail.com>
2018-02-22 18:02:26 -0500

    Increase the version for this feature release

--- primary-taxonomy-term.php
+++ primary-taxonomy-term.php
@@ -3,7 +3,7 @@
 Plugin Name: Primary Taxonomy Term
 Plugin URI: https://github.com/pressden/primary-taxonomy-term
 Description: Primary Taxonomy Term adds the ability to specify a primary term for any taxonomy.
-Version: 0.1.0
+Version: 0.2.0
 Author: D.S. Webster
 Author URI: http://pressden.com/
 License: GPLv3
